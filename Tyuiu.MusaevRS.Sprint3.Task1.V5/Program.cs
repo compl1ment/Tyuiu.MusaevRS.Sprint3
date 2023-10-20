9bb60b5a0ae6fc3b007345e6339b8d68e49ce6dd
Руслан Мусаев <ru.mu2005@mail.ru>
2023-10-20 21:49:35 +0500

    Task1

---
+++ Tyuiu.MusaevRS.Sprint3.Task1.V5/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.MusaevRS.Sprint3.Task1.V5.Lib;
+
+namespace Tyuiu.MusaevRS.Sprint3.Task1.V5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #2 | Выполнил Мусаев Р.Ш. | ПКТб-23-2";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: Оператор цикла while                                              *");
+            Console.WriteLine("* Задание №1                                                              *");
+            Console.WriteLine("* Вариант №5                                                              *");
+            Console.WriteLine("* Выполнил: Мусаев Руслан Шамхалович | ПКТб-23-2                          *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*Написать программу используя while, которая вычисляет произведение ряда  *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* StartValue = 1, StopValue = 10                                          *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            int startvalue = 1;
+            int stopvalue = 10;
+            double res = ds.GetMultiplySeries(startvalue, stopvalue);
+            Console.WriteLine(res);
+            Console.ReadKey();
+        }
+    }
+}

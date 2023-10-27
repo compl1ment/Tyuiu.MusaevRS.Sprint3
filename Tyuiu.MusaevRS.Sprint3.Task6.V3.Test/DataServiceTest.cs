6292b8f100e433be13a0c004666fffd46b9aab7c
Руслан Мусаев <ru.mu2005@mail.ru>
2023-10-27 19:37:36 +0300

    Task6

---
+++ Tyuiu.MusaevRS.Sprint3.Task6.V3.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.MusaevRS.Sprint3.Task6.V3.Lib;
+
+namespace Tyuiu.MusaevRS.Sprint3.Task6.V3.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int wait = 60;
+            int res = ds.GetSumTheDivisors(10, 14);
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.MusaevRS.Sprint3.Task6.V3.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.MusaevRS.Sprint3.Task6.V3.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.MusaevRS.Sprint3.Task6.V3.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a7d766e2-55de-4e23-9a77-4c86daac8f13")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
